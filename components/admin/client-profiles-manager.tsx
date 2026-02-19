e7b9e9b67ffa5263da7595820912c040bfd7ae73
teodelcastillo <teodorodelcastillop@gmail.com>
2026-02-19 13:52:21 -0300

    deployment fix

--- components/admin/client-profiles-manager.tsx
+++ components/admin/client-profiles-manager.tsx
@@ -99,7 +99,7 @@ export default function ClientProfilesManager() {
                         {client.name}
                       </CardTitle>
                       <Badge className={`mt-2 ${personTypeColors[client.person_type ?? 'other'] || ''}`}>
-                        {personTypeLabels[client.person_type ?? 'other'] || client.person_type ?? 'other'}
+                        {(personTypeLabels[client.person_type ?? 'other'] || client.person_type) ?? 'other'}
                       </Badge>
                     </div>
                   </div>
